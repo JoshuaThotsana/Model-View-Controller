ac9b8c99a7342054262d5e26eec4eb1ce919bff2
Thotsana <thotsana.mabotsa@umuzi.org>
2020-01-28 11:33:12 +0200

    second commit

---
+++ src/main/java/CalculatorController.java
@@ -0,0 +1,35 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorController {
+
+    private CalculatorView theView;
+    private CalculatorModel theModel;
+
+    public CalculatorController(CalculatorView theView, CalculatorModel theModel) {
+        this.theView = theView;
+        this.theModel = theModel;
+
+        this.theView.addCalculationListener(new CalculateListener());
+    }
+
+    class CalculateListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            int firstNumber, secondNumber = 0;
+
+            try {
+                firstNumber = theView.getFirstNumber();
+                theView.getSecondNumber();
+
+                theModel.addTwoNumbers(firstNumber, secondNumber);
+
+                theView.setCalcSolution(theModel.getCalculationValue());
+            } catch (NumberFormatException ex) {
+                theView.displayErrorMessage("You need to enter two Integers.");
+            }
+        }
+    }
+
+
+}
